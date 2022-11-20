d29d2eb4b868bcfe15516961b778f526bb493620
Владимир <soldatchenkovvolodya@gmail.com>
2022-11-20 23:42:41 +0500

    ывы

--- test1.py
+++ test1.py
@@ -1,9 +1,8 @@
 import pandas as pd
+
 df = pd.read_csv('C:/Users/solda/OneDrive/Документы/GitHub/data/Space_Corrected.csv')
 
-
-
-temp = df['Rocket'].dropna()
+temp = df['Rocket'].dropna()                                                        #преобразовываем стоимость миссии в цифры ( но нам это не надо )
 def cost_to_float(cost):
     cost = cost.replace(',', '')
     return float(cost)
@@ -18,17 +17,13 @@ def fill_rocket(rocket):
 df['Rocket'] = df['Rocket'].apply(fill_rocket)
 df['Rocket'].fillna(median_cost, inplace=True)
 
-
-
-def Country(c):
+def Country(c):                                                                     #достаем страну из локации
     c = c.split(',')
     c = c[-1]
     return c
 df["Location"] = df['Location'].apply(Country)
 
-
-
-def Status(stat):
+def Status(stat):                                                                   #говорим что если миссия прошла успешно, это 1. А если не успешно то это 0
     if stat == 'Success':
         return float(1)
     else:
@@ -36,10 +31,10 @@ def Status(stat):
 df['Status Mission'] = df['Status Mission'].apply(Status)
 # cортировка
 
-temp1 = df[df['Location'] == 'Russia']['Status Mission'].mean()
+temp1 = df[df['Location'] == 'Russia']['Status Mission'].mean()                     #находим средний показатель успешности запуска рокеты
 temp2 = df[df['Location'] != 'Russia']['Status Mission'].mean()
 
-temp1 = temp1*100
+temp1 = temp1*100                                                                       # преобразовывае в проценты
 temp2 = temp2*100
 
 
